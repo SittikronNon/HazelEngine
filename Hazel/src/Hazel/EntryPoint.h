420f9b5e538515e1db30328c8ea31b7d993e6717
SittikronNon <sittikron.lamulsab@gmail.com>
2022-07-02 18:29:12 +0700

    Update EntryPoint.h

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,10 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine\n");
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
+
 }
 
 #endif
